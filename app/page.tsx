cf9ca2f3a1cb5fde0c3fc5fde8649b1aae0a22d0
Bruno Serpa <brunospenna@gmail.com>
2023-11-07 14:47:35 -0300

    Routing and Navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main><h1>Hello Wolrd</h1></main>
+    <main>
+      <Link href="/users">Users</Link>
+      <h1>Hello Wolrd</h1>
+    </main>
   )
 }
